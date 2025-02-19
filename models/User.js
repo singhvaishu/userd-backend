5753e03abbb4652a1bfa7255542f4192b9406e61
Vaishali Singh <94376468+singhvaishu@users.noreply.github.com>
2025-02-19 23:35:53 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,16 +1,4 @@
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
 
-// const userSchema = new mongoose.Schema({
-//     email: { type: String, required: true, unique: true },
-//     password: { type: String, required: true },
-// });
-
-
-// module.exports = mongoose.model('User', userSchema);
-
-
-// backend/models/User.js
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
@@ -38,12 +26,6 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
-// Hash the password before saving the user
-UserSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
+
 
 module.exports = mongoose.model("User", UserSchema);
